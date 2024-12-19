637bdea4fa77a25e6e65fe731c05f00b385fdc19
OdaicuCristi <cristianpatrunsu163@gmail.com>
2024-12-19 13:34:04 +0200

    Add files via upload

---
+++ adivinanumero.js
@@ -0,0 +1,73 @@
+let randomNumber = Math.floor(Math.random()*100)+1;
+
+const guesses = document.querySelector(".guesses");
+const lastResult = document.querySelector(".lastResult");
+const lowOrHigh = document.querySelector(".lowOrHigh");
+
+const guessSubmit = document.querySelector(".guessSubmit");
+const guessField = document.querySelector(".guessField");
+
+let guessCount=1;
+let resetButton;
+
+function checkGuess(){
+    let userGuess= Number(guessField.value);
+    if(userGuess === randomNumber){
+        lastResult.textContent= "felicitari!";
+        lastResult.style.backgroundColor="green";
+        lowOrHigh.textContent="";
+        setGameOver();
+    }
+    else if (guessCount === 10) {
+        lastResult.textContent = "ESTI SLAB STAI ACASA";
+        setGameOver();
+    }
+    else {
+        lastResult.textContent = "Incorect";
+        lastResult.style.backgroundColor = "red";
+        if(userGuess < randomNumber) {
+            lowOrHigh.textContent = "Higher";
+        }
+        else if(userGuess > randomNumber) {
+            lowOrHigh.textContent = "Lower";
+        }
+    }
+     
+    guessCount++;
+    guessField.value="";
+    guessField.focus();
+
+}
+
+guessSubmit.addEventListener("click", checkGuess);
+function setGameOver(){
+    guessField.disabled=true;
+    guessSubmit.disabled=true;
+    resetButton = document.createElement("button");
+    resetButton.textContent= "Start new game";
+    resetButton.style.backgroundColor = "black";
+    resetButton.style.color="lightgreen";
+    resetButton.style.padding= "10px";
+    resetButton.style.border= "1px solid green";
+    resetButton.style.borderRadius="5px";
+    document.body.append(resetButton);
+    resetButton.addEventListener("click", resetGame);
+}
+
+function resetGame() {
+    guessCount=1;
+    const resetParas = document.querySelectorAll(".resultParas p");
+    for(let i=0; i<resetParas.length; i++){
+        resetParas[i].textContent ="";
+    }
+    
+    resetButton.parentNode.removeChild(resetButton);
+    guessField.disabled = false;
+    guessSubmit.disabled = false;
+    guessField.value = "";
+    guessField.focus();
+
+    lastResult.style.backgroundColor = "black";
+    randomNumber= Math.floor(Math.random()*100)+1;
+}
+
